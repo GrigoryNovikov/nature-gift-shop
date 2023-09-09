9813ccbb57f5e32a2e88303963fe7835ed305ce2
Ryhor Novikau <pegasmnsms@gmail.com>
2023-09-10 00:47:24 +0200

    catalog items

--- js/pendants.js
+++ js/pendants.js
@@ -2,20 +2,34 @@ let pendantElement = document.querySelectorAll('.pendants-item');
 let pendantElements = document.getElementById('pendantElements');
 let pendantsTitle = document.getElementsByTagName('h1');
 let backArrow = document.querySelector('.back-arrow');
+let pendant = document.querySelectorAll('.pendant')
 
-pendantElement.forEach((element) => {
-    element.addEventListener('click', function () {
-        pendantElements.style.display = "none";
-        pendantsTitle[0].style.display = "none";
-        document.getElementById('pendant1').style.display = ('flex');
-    });
-});
+pendantElements.addEventListener('click', function (element) {
+    let e = element.target;
+    let id = e.alt;
+    pendantElements.style.display = "none";
+    pendantsTitle[0].style.display = "none";
+    document.getElementById(id).style.display = ('flex');
+})
 
 backArrow.onclick = (function () {
-    document.getElementById('pendant1').style.display = ('none');
+    pendant.forEach((element) => {
+        element.style.display = 'none'
+    })
     pendantElements.style.display = "flex";
     document.getElementsByTagName('h1')[0].style.display = "block";
 })
+
+
+// pendantElement.forEach((element) => {
+//     element.addEventListener('click', function () {
+//         pendantElements.style.display = "none";
+//         pendantsTitle[0].style.display = "none";
+//         document.getElementById('pendant1').style.display = ('flex');
+//     });
+// });
+//
+
 
 $(document).ready(() => {
     $('.pendants-action__slider').slick({
